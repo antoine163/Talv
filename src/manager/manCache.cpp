e99248a0b3eefcbb30fd804db61a28af229ac4ab
antoine <antoine.maleyrie@gmail.com>
2014-02-01 08:13:02 +0100

    implemented ManCache but not tested

--- src/manager/manCache.cpp
+++ src/manager/manCache.cpp
@@ -14,6 +14,7 @@
 
 //WxWidgets
 #include <wx/dir.h>
+#include <wx/filefn.h> 
 #include <wx/stdpaths.h>
 #include <wx/arrstr.h>
 
@@ -34,23 +35,32 @@ ManCache::~ManCache()
 
 IMPLEMENT_MANAGER(ManCache);
 
-WinManager* ManCache::newEditWindow(wxWindow* parent)
+WinManager* ManCache::newEditWindow(wxWindow*)
 {
 	return nullptr;
 }
 
 Cache ManCache::getCache(wxLanguage const& lgsrc, wxLanguage const& lgto)
 {
-	return Cache();
+	return getCache(wxLocale::GetLanguageName(lgsrc)+"To"+wxLocale::GetLanguageName(lgto));
 }
 
 Cache ManCache::getCache(wxString const& name)
 {
-	return Cache();
+	Cache rCache;
+	
+	if(wxFileExists(name+".cac"))
+		rCache.setFileName(name+".cac");
+	
+	return rCache;
 }
 
 wxArrayString ManCache::getNameCaches()const
-{	
+{
+	wxArrayString files;
+	wxDir::GetAllFiles(_workDirectory, &files);
+	
+	return files;
 }
 
 void ManCache::workToTmp(bool toTmp, bool apply)
@@ -68,20 +78,28 @@ void ManCache::workToTmp(bool toTmp, bool apply)
 			wxDir::GetAllFiles(workDirectoryOld, &files);
 			
 			for(auto it: files)
-			{
-				
-			}
+				wxCopyFile(workDirectoryOld+"/"+it, _workDirectory+"/"+it);
 		}
 	}
 	//From Tmp
 	else if(_workInTmp && !toTmp)
 	{
+		wxString workDirectoryOld = _workDirectory;
+		_workDirectory = wxStandardPaths::Get().GetUserDataDir()+"/cache";
+		
 		if(apply)
 		{
+			wxDir::Remove(_workDirectory, wxPATH_RMDIR_FULL);
+			wxDir::Make(_workDirectory);
+			
+			wxArrayString files;
+			wxDir::GetAllFiles(workDirectoryOld, &files);
+			
+			for(auto it: files)
+				wxCopyFile(workDirectoryOld+"/"+it, _workDirectory+"/"+it);
 		}
 		
-		//wxDir::Remove(	,
-						//wxPATH_RMDIR_FULL);
+		wxDir::Remove(workDirectoryOld, wxPATH_RMDIR_FULL);
 	}
 	else
 		return;
